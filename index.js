e25852135509020129f517505d669359dd4fd635
Brooklynn Moor <brooklynn.moor@mediacurrent.com>
2020-07-06 22:04:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// route handler - get
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+})
+
+const PORT = process.env.PORT || 5000;
+
+// instructs express that node wants to listen for HTTP traffic at port 5000
+app.listen(PORT);
+
+//http://localhost:5000/

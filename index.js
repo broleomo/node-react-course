0774a8fd041b583be0637190ad3a9c19c4ae65bb
Brooklynn Moor <brooklynn.moor@mediacurrent.com>
2020-07-14 16:46:25 -0700

    adding passport info and oauth

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-// route handler - get
-app.get('/', (req, res) => {
-  res.send({name: 'Brooklynn. hey, how ya doin'});
-})
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
 // instructs express that node wants to listen for HTTP traffic at port 5000
 app.listen(PORT);
-
-//http://localhost:5000/
